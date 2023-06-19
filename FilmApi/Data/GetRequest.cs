5226ce78ef43c02de8e2b75df6d052b7d7fde166
AsterFM <asterfm2000@gmail.com>
2023-06-19 17:41:50 +0300

    5

---
+++ FilmApi/Data/GetRequest.cs
@@ -0,0 +1,33 @@
+﻿namespace FilmApi.Data;
+using System.Net;
+
+
+
+public class GetRequest
+{
+    HttpWebRequest _request;
+    string _adress;
+
+    public string Response { get; set; }
+
+    public GetRequest(string adress) 
+    {
+        _adress = adress;
+    }
+
+    public void Run() 
+    {
+        _request = (HttpWebRequest)WebRequest.Create(_adress);
+        _request.Method = "GET";
+        try
+        {
+            HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+            var stream = response.GetResponseStream();
+            if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+        }
+        catch (Exception) 
+        { 
+        }
+    }
+    
+}
---
+++ FilmApi/Data/Inforamtion.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmApi.Data
+{
+
+    public class Inforamtion
+    {
+        public int page = 1;
+        public string Name { get; set; }
+
+        public string Year { get; set; }
+
+        public string Img { get; set; }
+    }
+
+}
