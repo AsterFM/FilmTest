4f416036caad5760d8b29a7bc601e1e1d9105138
AsterFM <asterfm2000@gmail.com>
2023-06-22 11:54:11 +0300

    8

---
+++ FilmApi/Data/FilmPage.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Net.Http;
+
+namespace FilmApi.Data;
+
+public class FilmPage
+{
+    public class FilmItem
+    {
+        public int page = 1;
+        public string name_russian { get; set; }
+        public string year { get; set; }
+        public string big_poster { get; set; }
+    }
+
+    public FilmItem filmItem = new FilmItem();
+    
+    public List<FilmItem> Film(List<FilmItem> InfoList)
+    {
+        var webClient = new WebClient();
+        string jsonUri = "https://kinobd.ru/api/films?page=" + filmItem.page;
+
+        string jsonString =  webClient.DownloadString(jsonUri);
+
+        var jsonDocument = JsonDocument.Parse(jsonString);
+        var filmArray = jsonDocument.RootElement.GetProperty("data").EnumerateArray();
+
+        foreach (var filmElement in filmArray)
+        {
+            FilmItem Inf = new FilmItem();
+            FilmItem film = JsonSerializer.Deserialize<FilmItem>(filmElement.GetRawText());
+            Inf.name_russian = film.name_russian;
+            Inf.year = film.year;
+            Inf.big_poster = film.big_poster;
+            InfoList.Add(film);
+        }
+
+        return InfoList;
+    }
+}
--- FilmApi/Data/FullInfo.cs
+++ FilmApi/Data/FullInfo.cs
@@ -1,16 +1,52 @@
 ﻿namespace FilmApi.Data;
+using System.Net;
+using System.Text.Json;
 
 public class FullInfo
 {
-    public string Name { get; set; }
-    public string Desc { get; set; }
-    public string Year { get; set; }
-    public string RatingKP { get; set; }
-    public string RatingImdb { get; set; }
-    public string Duration { get; set; }
-    public string InRussian { get; set; }
-    public string Dudget { get; set; }
-    public string Pega { get; set; }
-    public string Country { get; set; }
-    public string BigImg { get; set; }
+
+    public class FilmItem
+    {
+        public string name_russian { get; set; }
+        public string description { get; set; }
+        public string year { get; set; }
+        public float rating_kp { get; set; }
+        public float rating_imdb { get; set; }
+        public string time { get; set; }
+        public string premiere_ru { get; set; }
+        public string budget { get; set; }
+        public string age_restriction { get; set; }
+        public string country_ru { get; set; }
+        public string big_poster { get; set; }
+    }
+    
+    public FullInfo.FilmItem Film(FilmItem FilmInfo, int page, int id)
+    {
+        FilmItem filmItem = new FilmItem();
+        var webClient = new WebClient();
+        string jsonUri = "https://kinobd.ru/api/films?page=" + page;
+
+        string jsonString =  webClient.DownloadString(jsonUri);
+
+        var jsonDocument = JsonDocument.Parse(jsonString);
+        var filmArray = jsonDocument.RootElement.GetProperty("data");
+        var filmElement = filmArray[id];
+        filmItem = JsonSerializer.Deserialize<FilmItem>(filmElement);
+        FilmInfo.name_russian = filmItem.name_russian;
+        FilmInfo.big_poster = filmItem.big_poster;
+        FilmInfo.description = filmItem.description;
+        FilmInfo.year = filmItem.year;
+        FilmInfo.rating_kp = filmItem.rating_kp;
+        FilmInfo.rating_imdb = filmItem.rating_imdb;
+        FilmInfo.time = filmItem.time;
+        FilmInfo.premiere_ru = filmItem.premiere_ru;
+        FilmInfo.budget = filmItem.budget;
+        FilmInfo.age_restriction = filmItem.age_restriction;
+        FilmInfo.country_ru = filmItem.country_ru;
+
+        return FilmInfo;
+    }
 }
+
+
+
